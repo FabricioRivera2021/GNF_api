2a792e3d564e2cc84f0a632315cf930f8a175af5
fabricio <fabricio.rivera2012@gmail.com>
2024-05-26 16:09:55 -0300

    add login y logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,28 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-        $data = $request->validate([
-            'name' => ['required', 'string'],
-            'email' => ['required', 'email', 'unique:users'],
-            'password' => ['required', 'min:6'],
-        ]);
+    // public function register(Request $request) {
+    //     $data = $request->validate([
+    //         'name' => ['required', 'string'],
+    //         'email' => ['required', 'email', 'unique:users'],
+    //         'password' => ['required', 'min:6'],
+    //     ]);
 
-        $user = User::create($data);
+    //     $user = User::create($data);
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+    //     $token = $user->createToken('auth_token')->plainTextToken;
 
-        return [
-            'user' => $user,
-            'token' => $token
-        ];    
-    }
-
-
+    //     return [
+    //         'user' => $user,
+    //         'token' => $token
+    //     ];    
+    // }
 
     public function login(Request $request) {
         $data = $request->validate([
@@ -35,17 +34,34 @@ class AuthController extends Controller
 
         $user = User::where('email', $data['email'])->first();
 
-        if(!$user || !Hash::check($data['password'], $user->password)){
+        if (Auth::attempt($data) && $user) {
+            // $request->session()->regenerate();
+            // return redirect()->intended('dashboard');
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return [
+                'user' => $user,
+                'token' => $token,
+                'message' => 'Login successful'
+            ];
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+    
+    public function logout(Request $request) {
+        /** @var User $user */
+        $user = Auth::user();
+        if(!$user){
             return response([
-                'message' => 'Bad creds'
-            ], 401);
+                'sccess' => false
+            ], 404);
         }
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+        $userOk = User::where('id', $user->id)->get();
+        // Revoke the token that was used to authenticate the current request...
+        // $userOk->currentAccessToken()->delete();
 
         return [
-            'user' => $user,
-            'token' => $token
+            'message' => 'Logout successful'
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,19 @@ use App\Http\Controllers\NumerosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post("/register", [AuthController::class, "register"]);
+// Route::post("/register", [AuthController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
+// Route::post("/logout", [AuthController::class, "logout"]);
+
+Route::get("/dashboard");
 
 Route::post("/createNumber", [NumerosController::class, "createNumber"]);
 Route::post("/createNumberFromSW", [NumerosController::class, "createNumberFromSW"]);
