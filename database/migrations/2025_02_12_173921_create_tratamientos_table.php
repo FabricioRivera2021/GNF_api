2b334b26e4982d9661e2400199c46eda20b60035
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2025-02-14 11:53:56 -0300

    añadiendo datos a retiros ttos

--- database/migrations/2025_02_12_173921_create_tratamientos_table.php
+++ database/migrations/2025_02_12_173921_create_tratamientos_table.php
@@ -15,15 +15,24 @@ return new class extends Migration
             $table->id();
             $table->date('fecha_inicio');
             $table->date('fecha_fin');
+            $table->unsignedBigInteger('tto_dias');
             $table->unsignedBigInteger('medicos_id');
             $table->unsignedBigInteger('medication_id');
-            $table->timestamps();
-            $table->unsignedBigInteger('total_tto');
-            $table->unsignedBigInteger('retiros_pendientes');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('activo');
+            $table->unsignedBigInteger('total_tto_dias');
+            $table->unsignedBigInteger('total_tto_dias_pendientes');
             $table->unsignedBigInteger('retiros_por_mes');
+            $table->unsignedBigInteger('retiros_pendientes');
+            $table->enum('tipo_tto', ['cronico', 'agudo', 'FNR', 'compra_especial']);
             $table->foreign('medicos_id')->references('id')->on('medicos')
             ->onDelete('cascade');
             $table->foreign('medication_id')->references('id')->on('medications')
+            ->onDelete('cascade');
+            $table->foreign('customer_id')->references('id')->on('customers')
+            ->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
             ->onDelete('cascade');
         });
     }
