61436b97feaf6afc3d1af1c2c5e57087224220aa
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2025-04-11 18:38:33 -0300

    factory de tratamientos

--- database/factories/TratamientosFactory.php
+++ database/factories/TratamientosFactory.php
@@ -16,8 +16,31 @@ class TratamientosFactory extends Factory
      */
     public function definition(): array
     {
+        $isShortTreatment = $this->faker->boolean(50);
+        $totalDays = $isShortTreatment
+            ? $this->faker->numberBetween(1, 30)
+            : $this->faker->numberBetween(30, 180); // 1 to 6 months
+        $startDate = $this->faker->dateTimeBetween('-1 year', 'now');
+        $endDate = (clone $startDate)->modify("+{$totalDays} days");
+        $ttoDiasMes = $this->faker->numberBetween(1, 10);
+        $retirosMes = $this->faker->numberBetween(1, 3);
+        $diasPendientes = $this->faker->numberBetween(0, $totalDays);
+        $retirosPendientes = $this->faker->numberBetween(0, $retirosMes);
+    
         return [
-            //
+            'fecha_inicio' => $startDate->format('Y-m-d'),
+            'fecha_fin' => $endDate->format('Y-m-d'),
+            'tto_dias_mes' => $ttoDiasMes,
+            'medicos_id' => $this->faker->numberBetween(1, 10),
+            'medication_id' => $this->faker->numberBetween(1, 10),
+            'customer_id' => 1,
+            'user_id' => $this->faker->numberBetween(1, 5),
+            'activo' => $this->faker->boolean(80),
+            'total_tto_dias' => $totalDays,
+            'total_tto_dias_pendientes' => $diasPendientes,
+            'tipo_tto' => $this->faker->randomElement(['crónico']),
+            'retiros_por_mes' => $retirosMes,
+            'retiros_pendientes' => $retirosPendientes,
         ];
     }
 }
